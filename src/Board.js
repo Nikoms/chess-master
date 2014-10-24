28e39b3943a09fd71aa30911e354654351683a2b
nikoms <nikoms@gmail.com>
2014-10-24 14:17:48 +0200

    A board has now a size! :)

--- src/Board.js
+++ src/Board.js
@@ -11,4 +11,13 @@ Board.prototype.getWidth = function () {
     return this.width;
 };
 
+Board.prototype.getWidthToInt = function () {
+    return 1 + this.width.charCodeAt(0) - 'A'.charCodeAt(0);
+}
+
+
+Board.prototype.getSize = function () {
+    return this.getWidthToInt() * this.height;
+};
+
 module.exports = Board;
--- test/testBoard.js
+++ test/testBoard.js
@@ -13,4 +13,14 @@ describe('Board', function () {
             assert.strictEqual('H', board.getWidth());
         })
     });
+    describe('#getWidthToInt', function () {
+        it('should return 8 (because H = 8)', function () {
+            assert.strictEqual(8, board.getWidthToInt());
+        })
+    });
+    describe('#getSize', function () {
+        it('should return 8XH = 64', function () {
+            assert.strictEqual(64, board.getSize());
+        })
+    });
 });
