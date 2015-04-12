ce69616a880ff9671153d8fe22b114385bd36618
nikoms <nikoms@gmail.com>
2015-04-12 21:45:10 +0200

    Add the possibility to have other pieces represented by pawns

--- test/BoardMoveTester.js
+++ test/BoardMoveTester.js
@@ -3,6 +3,7 @@ var Position = require('../src/Position');
 var assert = require("assert");
 var Board = require('../src/Board.js');
 var Game = require('../src/Game.js');
+var Pawn = require('../src/Move/Pawn.js');
 //Example: X = possible move, 0 = current position, # = free position
 //var movesRepresentation = [
 //    'X###X',
@@ -86,14 +87,33 @@ BoardMoveTester.prototype.getPossibleMoves = function () {
     }
     return possibleMoves;
 };
+
+
+BoardMoveTester.prototype.getOtherPiecesPositions = function () {
+    var positions = [];
+    for (var height = 0; height < this.height; height++) {
+        for (var width = 0; width < this.width; width++) {
+            var currentCharacter = this.movesRepresentation[height][width];
+            if (currentCharacter === 'P') {
+                positions.push(new Position(width, this.convertHeightToPosition(height)));
+            }
+        }
+    }
+    return positions;
+};
+
 BoardMoveTester.prototype.getPositionRange = function () {
     return new PositionRange(new Position(0, 0), new Position(this.width - 1, this.height - 1));
 };
 
 BoardMoveTester.prototype.getGame = function () {
-    return new Game(new Board(this.getPositionRange()));
+    var game = new Game(new Board(this.getPositionRange()));
+    var otherPiecesPositions = this.getOtherPiecesPositions();
+    for (var key in otherPiecesPositions) {
+        game.addPiece(otherPiecesPositions[key], new Pawn());
+    }
+    return game;
 };
-
 
 
 module.exports = BoardMoveTester;
