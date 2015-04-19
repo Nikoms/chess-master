58818f2218d67ea53740afd3815f4d3836ed4eda
nikoms <nikoms@gmail.com>
2015-04-19 23:46:18 +0200

    The knight can go to an enemy's position

--- src/Move/Knight.js
+++ src/Move/Knight.js
@@ -1,35 +1,45 @@
 var Position = require('../Position.js');
+var Piece = require('../Piece');
+var util = require("util");
 
-function addPositionIfValid(positions, currentPosition, addendX, addendY, board){
-    var newPosition = currentPosition.addX(addendX).addY(addendY);
-    if(board.isPositionValid(newPosition)){
-        positions.push(newPosition);
-    }
+function Knight(color) {
+    Piece.apply(this, arguments);
 }
 
+util.inherits(Knight, Piece);
 
-
-function Knight() {
+function _addPositionIfValid(positions, currentPosition, addendX, addendY, game){
+    var newPosition = currentPosition.addX(addendX).addY(addendY);
+    if(game.board.isPositionValid(newPosition)){
+        if(game.isPositionFree(newPosition)){
+            positions.push(newPosition);
+        }else{
+            if(game.getPiece(newPosition).getColor() !== this.getColor()){
+                positions.push(newPosition);
+            }
+        }
+    }
 }
 
 Knight.prototype.getPossibleMoves = function (game, currentPosition){
     var positions = [];
+    var addPositionIfValid = _addPositionIfValid.bind(this);
 
     //Bottom left
-    addPositionIfValid(positions, currentPosition, -2, -1, game.board);
-    addPositionIfValid(positions, currentPosition, -1, -2, game.board);
+    addPositionIfValid(positions, currentPosition, -2, -1, game);
+    addPositionIfValid(positions, currentPosition, -1, -2, game);
 
     //Bottom right
-    addPositionIfValid(positions, currentPosition, 2, -1, game.board);
-    addPositionIfValid(positions, currentPosition, 1, -2, game.board);
+    addPositionIfValid(positions, currentPosition, 2, -1, game);
+    addPositionIfValid(positions, currentPosition, 1, -2, game);
 
     //Top left
-    addPositionIfValid(positions, currentPosition, -1, 2, game.board);
-    addPositionIfValid(positions, currentPosition, -2, 1, game.board);
+    addPositionIfValid(positions, currentPosition, -1, 2, game);
+    addPositionIfValid(positions, currentPosition, -2, 1, game);
 
     //Top right
-    addPositionIfValid(positions, currentPosition, 1, 2, game.board);
-    addPositionIfValid(positions, currentPosition, 2, 1, game.board);
+    addPositionIfValid(positions, currentPosition, 1, 2, game);
+    addPositionIfValid(positions, currentPosition, 2, 1, game);
 
     return positions;
 };
--- test/Move/KnightTest.js
+++ test/Move/KnightTest.js
@@ -1,8 +1,9 @@
 var Knight = require('../../src/Move/Knight.js');
 var BoardMoveTester = require('../BoardMoveTester.js');
+var Piece = require('../../src/Piece');
 
 describe('Knight', function () {
-    var knight = new Knight();
+    var knight = new Knight(Piece.WHITE);
 
     describe('#getPossibleMoves', function () {
         it('The knight can move to a square that is two squares horizontally and one square vertically, or two squares vertically and one square horizontally.', function () {
@@ -26,13 +27,23 @@ describe('Knight', function () {
 
         });
 
-        it('The knight can jump over a piece (this test is not optional).', function () {
+        it('The knight can jump over a partner piece and cant go where an partner stands', function () {
 
             var moveRepresentation = [
                 '##X#X#',
                 '#XWWWX',
                 '##P0W#',
+                '#XWWWW',
+                '##X#X#'];
+            new BoardMoveTester(moveRepresentation).assertPossibleMoves(knight);
+        });
+        it('The knight can jump over pieces and can take an enemy.', function () {
+
+            var moveRepresentation = [
+                '##X#X#',
                 '#XWWWX',
+                '##P0W#',
+                '#XWWWB',
                 '##X#X#'];
             new BoardMoveTester(moveRepresentation).assertPossibleMoves(knight);
         });
